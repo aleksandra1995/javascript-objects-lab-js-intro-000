86d099e9118bfa7f27528a16755ee6249f91ebed
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-04 20:01:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {prop: '1'}
+var recipes = {[key]: value}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value }) }
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) { object[key] = value
